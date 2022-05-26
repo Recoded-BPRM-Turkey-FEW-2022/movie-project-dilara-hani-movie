461eb21d4c42ef800fecad7c7e8b61be6e6d4794
Dilara Katuk <dilarakatuk@gmail.com>
2022-05-27 00:14:21 +0300

    search functionality for movie queries

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 'use strict';
-
+//FOR URL GENERATION
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+//FOR PAGE CONTENT
 const CONTAINER = document.querySelector("#page-content"); // changed class name .container to id #page-content
 CONTAINER.classList = ("container-content container-fluid d-flex flex-row flex-wrap justify-content-center text-center mt-4")
-
-// Don't touch this function please
-const autorun = async () => {
-  const movies = await fetchMovies();
-  renderMovies(movies.results);
-};
-
-const homeButton = async () => {
-  CONTAINER.innerHTML="";
-  const movies = await fetchMovies();
-  renderMovies(movies.results);
-}
+//FOR SEARCH BAR
+const searchInput = document.getElementById("search-input") 
+const searchButton = document.getElementById("search-button") 
 
 // Don't touch this function please
 const constructUrl = (path) => {
@@ -24,6 +16,65 @@ const constructUrl = (path) => {
     "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
   )}`;
 };
+
+
+// SEARCH BUTTON FUNCTIONALITY
+
+// CREATE SEARCH URL WITH QUERY
+const searchUrl = (query) => {
+  console.log(query)
+  return `${TMDB_BASE_URL}/search/multi?api_key=${atob(
+    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
+  )}&query=${query}`;
+
+};
+searchButton.addEventListener("click", async (e) =>{ // without async await the promise is not resolved.
+  e.preventDefault();
+  let query = searchInput.value // what is written into input bar
+  console.log(query)
+  let movies = await searchMovies(query)
+  // console.log(searchMovies(query))  //searchMovies returns a promise.
+  console.log(movies) // returns an object
+  CONTAINER.innerHTML ="";
+  renderMovies(movies.results)
+  // searchUrl(query)
+})
+
+const searchMovies = async (query) => {
+  const url = searchUrl(query);
+  const res = await fetch(url);
+  const result = res.json();
+  console.log(result)
+  return result;
+};
+
+
+// MOVIES PAGE
+const renderMovies = (movies) => {
+  movies.map((movie) => {
+    const movieDiv = document.createElement("div");
+    movieDiv.classList =("col-lg-3 col-md-4 col-sm-12 m-3 p-0 d-flex flex-column")
+    movieDiv.id =("movieDivCard")
+    movieDiv.innerHTML = `
+    <div class="card w-100 h-100" id="movieCards">
+      <img class="card-img-top" src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title} poster">
+
+        <h4>${movie.title}</h4>
+           
+      <div class="card-body" id="movieOverview">
+        <h4 id="overview">Overview</h4>
+        <p> ${movie.overview}</p>
+      </div>
+    </div>
+        `;
+        
+    movieDiv.addEventListener("click", () => {
+      movieDetails(movie);
+    });
+    CONTAINER.appendChild(movieDiv);
+  });
+};
+
 
 // You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
@@ -43,32 +94,6 @@ const fetchMovie = async (movieId) => {
   const url = constructUrl(`movie/${movieId}`);
   const res = await fetch(url);
   return res.json();
-};
-
-// You'll need to play with this function in order to add features and enhance the style.
-const renderMovies = (movies) => {
-  movies.map((movie) => {
-    const movieDiv = document.createElement("div");
-    movieDiv.classList =("col-lg-3 col-md-4 col-sm-12 m-3 p-0 d-flex flex-column")
-    movieDiv.id =("movieDivCard")
-    movieDiv.innerHTML = `
-    <div class="card w-100 h-100" id="movieCards">
-      <img class="card-img-top" src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster">
-      <h3>${movie.title}</h3>
-      <div class="card-body" id="movieOverview">
-        <h4 id="overview">Overview</h4>
-        <p> ${movie.overview}</p>
-      </div>
-    </div>
-        `;
-        
-    movieDiv.addEventListener("click", () => {
-      movieDetails(movie);
-    });
-    CONTAINER.appendChild(movieDiv);
-  });
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
@@ -93,6 +118,19 @@ const renderMovie = (movie) => {
             <h3>Actors:</h3>
             <ul id="actors" class="list-unstyled"></ul>
     </div>`;
+};
+
+// HOME BUTTON FUNCTIONALITY
+const homeButton = async () => {
+  CONTAINER.innerHTML="";
+  const movies = await fetchMovies();
+  renderMovies(movies.results);
+}
+
+// Don't touch this function please
+const autorun = async () => {
+  const movies = await fetchMovies();
+  renderMovies(movies.results);
 };
 
 document.addEventListener("DOMContentLoaded", autorun);
